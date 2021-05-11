b2c9d7db9a4d65821764f567f283b9ad3ddaeb2a
KevinArmache <Kevinarmache@gmail.com>
2021-05-11 16:34:19 +0100

    feat: work on categories

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,45 +1,66 @@
 import { useState, useEffect } from "react";
+import Pagination from "../Components/Pagination.jsx";
+function Category() {
+  // CATEGORY
+  const [Category, setCategory] = useState([]);
+  useEffect(function () {
+    fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=4521ea8693f4def400a9777ba95e0bc2&language=fr-FR"
+    )
+      .then((res) => res.json())
+      .then(function (data) {
+        let Category = data.genres.map(function (element) {
+          return (
+            <button
+              type="button"
+              key={element.id}
+              onClick={handleclickCategories}
+              class="btn btn-outline-light"
+            >
+              {element.name}
+            </button>
+          );
+        });
 
-function Category () {
-    const [Category, setCategory] = useState([]);
-    useEffect(function () {
-  fetch(
-    "https://api.themoviedb.org/3/genre/movie/list?api_key=4521ea8693f4def400a9777ba95e0bc2&language=en-US"
-  )
-    .then((res) => res.json())
-    .then(function (data) {
-    let Category = data.genres.map(function (element){
-    return <button type="button" class="btn btn-outline-light">{element.name}</button>
-        
-    })
-    setCategory(Category)
-    });
-}, []);
-// https://api.themoviedb.org/3/discover/movie?api_key=4521ea8693f4def400a9777ba95e0bc2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
-const [Discovery, setDiscovery] = useState([]);
-useEffect(function () {
-    let TableDiscovery= []
+        setCategory(Category);
+      });
+  }, []);
 
-    for(let i=1; i <)
-fetch(
-" https://api.themoviedb.org/3/discover/movie?api_key=4521ea8693f4def400a9777ba95e0bc2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
-)
-.then((res) => res.json())
-.then(function (data) {
-let Category = data.genres.map(function (element){
-return <button type="button" class="btn btn-outline-light">{element.name}</button>
-    
-})
-setCategory(Category)
-});
-}, []);
+  // DISCOCOVERY
 
-return (
-    <div>
-        <div className="Category">
-        {Category}
-        </div>
-    </div>
-)
+  const [Discovery, setDiscovery] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=4521ea8693f4def400a9777ba95e0bc2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+    )
+      .then((res) => res.json())
+      .then(function (data) {
+        return setDiscovery(data.results);
+      });
+  }, []);
+
+  function handleclickCategories() {
+    return (
+      <>
+        {
+          (console.log("Categories : ", Category),
+          console.log("Discovery : ", Discovery))
+        }
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        <div className="Category">{Category}</div>
+      </div>
+
+      <div>
+        <Pagination />
+      </div>
+    </>
+  );
 }
-export default Category
+export default Category;
